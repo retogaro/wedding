349007424e7481977a25953545bcad201afaecd3
Reto Garo <reto.garo@gmail.com>
2020-06-10 13:03:23 +1000

    Add Javascript for Modal on RSVP button

---
+++ app/routables/index/controller.js
@@ -0,0 +1,22 @@
+import Controller from "@ember/controller";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+
+export default class IndexController extends Controller {
+  queryParams = ["modal"];
+
+  @tracked modal = null;
+  @tracked showModal = false;
+
+  @action
+  openModal() {
+    console.log("openModal");
+    this.showModal = true;
+  }
+
+  @action
+  closeModal() {
+    console.log("closeModal");
+    this.showModal = false;
+  }
+}
